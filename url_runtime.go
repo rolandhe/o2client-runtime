0d4ae17713c23f21a089212f456afd0337670a10
hexiufeng <rolandhe@qq.com>
2023-10-24 20:16:37 +0800

    add simple struct slice support

--- url_runtime.go
+++ url_runtime.go
@@ -36,9 +36,7 @@ func (w *wrapUrl) AddParam(name string, value string) {
 }
 
 func (w *wrapUrl) AddParamWithValues(name string, values []string) {
-	for _, v := range values {
-		w.query.Add(name, v)
-	}
+	w.query[name] = values
 }
 
 func (w *wrapUrl) AddSimpleParamObject(simpleParam any) error {
@@ -56,24 +54,22 @@ func (w *wrapUrl) AddSimpleParamObject(simpleParam any) error {
 		if name == "" {
 			name = field.Name
 		}
-		if fValue.Kind() == reflect.String {
-			w.query.Add(name, fValue.String())
-			continue
-		}
-		if fValue.Kind() == reflect.Bool {
-			w.query.Add(name, strconv.FormatBool(fValue.Bool()))
-			continue
-		}
-		if fValue.CanInt() {
-			w.query.Add(name, strconv.FormatInt(fValue.Int(), 10))
-			continue
-		}
-		if fValue.CanUint() {
-			w.query.Add(name, strconv.FormatUint(fValue.Uint(), 10))
+		if strValue, ok := w.maybeSimple(fValue); ok {
+			w.query.Add(name, strValue)
 			continue
 		}
 
-		return errors.New("get simpleParam just support int,uint and string")
+		if fValue.Kind() == reflect.Slice {
+			if fValue.Len() == 0 {
+				continue
+			}
+			if err := w.acceptSlice(fValue, name); err != nil {
+				return err
+			}
+			continue
+		}
+
+		return errors.New("get simpleParam just support int,uint and string, array of these simple type")
 
 	}
 	return nil
@@ -82,4 +78,34 @@ func (w *wrapUrl) AddSimpleParamObject(simpleParam any) error {
 func (w *wrapUrl) ToString() string {
 	w.url.RawQuery = w.query.Encode()
 	return w.url.String()
+}
+
+func (w *wrapUrl) maybeSimple(fValue reflect.Value) (string, bool) {
+	if fValue.Kind() == reflect.String {
+		return fValue.String(), true
+	}
+	if fValue.Kind() == reflect.Bool {
+		return strconv.FormatBool(fValue.Bool()), true
+	}
+	if fValue.CanInt() {
+		return strconv.FormatInt(fValue.Int(), 10), true
+	}
+	if fValue.CanUint() {
+		return strconv.FormatUint(fValue.Uint(), 10), true
+	}
+	return "", false
+}
+
+func (w *wrapUrl) acceptSlice(fValue reflect.Value, name string) error {
+	var values []string
+	for i := 0; i < fValue.Len(); i++ {
+		v := fValue.Index(i)
+		if strValue, ok := w.maybeSimple(v); ok {
+			values = append(values, strValue)
+		} else {
+			return errors.New("slice element just support int,uint and string")
+		}
+	}
+	w.AddParamWithValues(name, values)
+	return nil
 }
--- url_runtime_test.go
+++ url_runtime_test.go
@@ -13,11 +13,13 @@ type Param struct {
 func TestAddObjectParam(t *testing.T) {
 	w, _ := NewWrapUrl("http://xx-service:8080")
 	param := &struct {
-		Name string `json:"name"`
-		Id   int64  `json:"id"`
+		Name  string  `json:"name"`
+		Id    int64   `json:"id"`
+		Kinds []int64 `json:"kinds"`
 	}{
-		Name: "Joe",
-		Id:   101,
+		Name:  "Joe",
+		Id:    101,
+		Kinds: []int64{1, 2, 3},
 	}
 
 	err := w.AddSimpleParamObject(param)
